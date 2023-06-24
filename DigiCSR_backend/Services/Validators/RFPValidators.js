a30d8943e16d92be7a5327d5c6f48765be770a23
AlvishVarsani <alvishvarsani2003@gmail.com>
2023-06-24 15:15:45 +0530

    alvish

--- DigiCSR_backend/Services/Validators/RFPValidators.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-exports.RFPValidator = Joi.object({
-  title: Joi.string().required(),
-  amount: Joi.number().required(),
-  sectors: Joi.array().items(Joi.string().required()).required(),
-  states: Joi.array().items(Joi.string().required()).required(),
-  timeline: Joi.string().required(),
-});
